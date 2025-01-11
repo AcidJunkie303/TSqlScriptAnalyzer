817bf4904c5e154963bd2c0917b47e15a531e50d
AcidJunkie303 <thomas_binggeli@yahoo.com>
2025-01-11 07:06:05 +0100

    added missing property descriptions

--- src/src/DatabaseAnalyzers.DefaultAnalyzers/Analyzers/Settings/Aj5045Settings.cs
+++ src/src/DatabaseAnalyzers.DefaultAnalyzers/Analyzers/Settings/Aj5045Settings.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using DatabaseAnalyzer.Contracts;
 
 namespace DatabaseAnalyzers.DefaultAnalyzers.Analyzers.Settings;
@@ -16,7 +17,9 @@ internal sealed class Aj5045SettingsRaw : IRawSettings<Aj5045Settings>
 }
 
 internal sealed record Aj5045Settings(
+    [property: Description("An empty line is required before a GO statement. Default is false.")]
     bool RequireEmptyLineBeforeGo,
+    [property: Description("An empty line is required after a GO statement. Default is false.")]
     bool RequireEmptyLineAfterGo
 ) : ISettings<Aj5045Settings>
 {
