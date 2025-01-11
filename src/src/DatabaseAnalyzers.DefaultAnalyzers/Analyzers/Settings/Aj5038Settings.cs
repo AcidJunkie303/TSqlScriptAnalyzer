5d1aada0fd86cdd0814e7b2a84993387aed09109
AcidJunkie303 <thomas_binggeli@yahoo.com>
2025-01-11 06:09:28 +0100

    updated docs

--- src/src/DatabaseAnalyzers.DefaultAnalyzers/Analyzers/Settings/Aj5038Settings.cs
+++ src/src/DatabaseAnalyzers.DefaultAnalyzers/Analyzers/Settings/Aj5038Settings.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using DatabaseAnalyzer.Contracts;
 using DatabaseAnalyzers.DefaultAnalyzers.Analyzers.Naming;
 
@@ -23,10 +24,15 @@ internal sealed class Aj5038SettingsRaw : IRawSettings<Aj5038Settings>
 }
 
 internal sealed record Aj5038Settings(
+    [property: Description("Defines the name quoting policy for object creations.")]
     NameQuotingPolicy NameQuotingPolicyDuringObjectCreation,
+    [property: Description("Defines the name quoting policy for column references.")]
     NameQuotingPolicy NameQuotingPolicyForColumnReferences,
+    [property: Description("Defines the name quoting policy for column definitions during creation.")]
     NameQuotingPolicy NameQuotingPolicyForColumnDefinitions,
+    [property: Description("Defines the name quoting policy for table references.")]
     NameQuotingPolicy NameQuotingPolicyForTableReferences,
+    [property: Description("Defines the name quoting policy for data types.")]
     NameQuotingPolicy NameQuotingPolicyForDataTypes
 ) : ISettings<Aj5038Settings>
 {
