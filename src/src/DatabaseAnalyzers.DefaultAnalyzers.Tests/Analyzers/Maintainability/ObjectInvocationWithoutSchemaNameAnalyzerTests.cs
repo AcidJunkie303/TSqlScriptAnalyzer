255733d77db0f0e6be336f8923e40fce6b3cda5d
AcidJunkie303 <thomas_binggeli@yahoo.com>
2025-01-19 09:27:14 +0100

    False positive in object invocation without explicitly specified schema name when calling builtin functions (#8)

--- src/src/DatabaseAnalyzers.DefaultAnalyzers.Tests/Analyzers/Maintainability/ObjectInvocationWithoutSchemaNameAnalyzerTests.cs
+++ src/src/DatabaseAnalyzers.DefaultAnalyzers.Tests/Analyzers/Maintainability/ObjectInvocationWithoutSchemaNameAnalyzerTests.cs
@@ -8,11 +8,6 @@ namespace DatabaseAnalyzers.DefaultAnalyzers.Tests.Analyzers.Maintainability;
 public sealed class ObjectInvocationWithoutSchemaNameAnalyzerTests(ITestOutputHelper testOutputHelper)
     : ScriptAnalyzerTestsBase<ObjectInvocationWithoutSchemaNameAnalyzer>(testOutputHelper)
 {
-    private static readonly Aj5049Settings IgnoredBuiltInFunctions = new Aj5049SettingsRaw
-    {
-        IgnoredObjectNamePatterns = ["ABS", "STRING_SPLIT"]
-    }.ToSettings();
-
     [Fact]
     public void WithUserCreatedProcedure_WhenInvokingWithSchemaName_ThenOk()
     {
@@ -33,30 +28,6 @@ public sealed class ObjectInvocationWithoutSchemaNameAnalyzerTests(ITestOutputHe
                             GO
 
                             EXEC ▶️AJ5049💛script_0.sql💛💛procedure💛MyProcedure✅MyProcedure◀️ @Id = 303
-                            """;
-        Verify(Aj5049Settings.Default, code);
-    }
-
-    [Fact]
-    public void WithUserCreatedScalarFunction_WhenInvokingWithSchemaName_ThenOk()
-    {
-        const string code = """
-                            USE MyDb
-                            GO
-
-                            SELECT dbo.MyScalarFunc() AS MyValue
-                            """;
-        Verify(Aj5049Settings.Default, code);
-    }
-
-    [Fact]
-    public void WithUserCreatedScalarFunction_WhenInvokingWithoutSchemaName_ThenDiagnose()
-    {
-        const string code = """
-                            USE MyDb
-                            GO
-
-                            SELECT ▶️AJ5049💛script_0.sql💛💛scalar function💛MyScalarFunc✅MyScalarFunc()◀️ AS MyValue
                             """;
         Verify(Aj5049Settings.Default, code);
     }
@@ -83,30 +54,5 @@ public sealed class ObjectInvocationWithoutSchemaNameAnalyzerTests(ITestOutputHe
                             SELECT * FROM  ▶️AJ5049💛script_0.sql💛💛table valued function💛MyTableValuedFunction✅MyTableValuedFunction()◀️
                             """;
         Verify(Aj5049Settings.Default, code);
-    }
-
-    [Fact]
-    public void WithBuiltInScalarFunction_WhenNotIgnored_WhenInvokedWithoutSchema_ThenReport()
-    {
-        const string code = """
-                            USE MyDb
-                            GO
-
-                            SELECT ▶️AJ5049💛script_0.sql💛💛scalar function💛ABS✅ABS(-10)◀️ AS AbsoluteValue
-                            """;
-        Verify(Aj5049Settings.Default, code);
-    }
-
-    [Fact]
-    public void WithBuiltInScalarFunction_WhenIgnored_WhenInvokedWithoutSchema_ThenOk()
-    {
-        const string code = """
-                            USE MyDb
-                            GO
-
-                            SELECT ABS(-10) AS AbsoluteValue
-                            """;
-
-        Verify(IgnoredBuiltInFunctions, code);
     }
 }
