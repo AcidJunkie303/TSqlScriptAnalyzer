501229d85743dd9d34138c724daf838e67f6f7af
AcidJunkie303 <thomas_binggeli@yahoo.com>
2025-02-25 19:54:52 +0100

    fixed

--- src/src/DatabaseAnalyzers.DefaultAnalyzers/Analyzers/Runtime/MissingObjectAnalyzer.cs
+++ src/src/DatabaseAnalyzers.DefaultAnalyzers/Analyzers/Runtime/MissingObjectAnalyzer.cs
@@ -1,4 +1,7 @@
+using System.Collections.Frozen;
+using System.Diagnostics.CodeAnalysis;
 using DatabaseAnalyzer.Common.Extensions;
+using DatabaseAnalyzer.Common.Services;
 using DatabaseAnalyzer.Common.SqlParsing;
 using DatabaseAnalyzer.Common.SqlParsing.Extraction;
 using DatabaseAnalyzer.Common.SqlParsing.Extraction.Models;
@@ -8,6 +11,7 @@ using Microsoft.SqlServer.TransactSql.ScriptDom;
 
 namespace DatabaseAnalyzers.DefaultAnalyzers.Analyzers.Runtime;
 
+[SuppressMessage("Major Code Smell", "S1200:Classes should not be coupled to too many other classes")]
 public sealed class MissingObjectAnalyzer : IGlobalAnalyzer
 {
     public IReadOnlyList<IDiagnosticDefinition> SupportedDiagnostics => [DiagnosticDefinitions.Default];
@@ -18,12 +22,18 @@ public sealed class MissingObjectAnalyzer : IGlobalAnalyzer
         var databasesByName = new DatabaseObjectExtractor(context.IssueReporter)
             .Extract(context.Scripts, context.DefaultSchemaName);
 
+        var cteNamesByBatch = context.Scripts
+            .SelectMany(a => a.ParsedScript.Batches)
+            .ToDictionary(a => a, CteExtractor.ExtractCteNames);
+
+        var cteBatchInformationProvider = new CteBatchInformationProvider(cteNamesByBatch);
+
         AnalyzeProcedureCalls(context, settings, databasesByName);
-        AnalyzeTableReferences(context, settings, databasesByName);
-        AnalyzeColumnReferences(context, settings, databasesByName);
+        AnalyzeTableReferences(context, settings, databasesByName, cteBatchInformationProvider);
+        AnalyzeColumnReferences(context, settings, databasesByName, cteBatchInformationProvider);
     }
 
-    private static void AnalyzeColumnReferences(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName)
+    private static void AnalyzeColumnReferences(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName, CteBatchInformationProvider cteBatchInformationProvider)
     {
         var columnReferencesAndScripts = context.Scripts
             .SelectMany(a => a
@@ -34,17 +44,32 @@ public sealed class MissingObjectAnalyzer : IGlobalAnalyzer
 
         foreach (var (columnReference, script) in columnReferencesAndScripts)
         {
-            AnalyzeColumnReference(context, settings, databasesByName, script, columnReference);
+            AnalyzeColumnReference(context, settings, databasesByName, script, columnReference, cteBatchInformationProvider);
         }
     }
 
-    private static void AnalyzeColumnReference(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName, IScriptModel script, ColumnReferenceExpression columnReference)
+    private static void AnalyzeColumnReference(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName, IScriptModel script, ColumnReferenceExpression columnReference, CteBatchInformationProvider cteBatchInformationProvider)
     {
         var resolver = new TableColumnResolver(new IssueReporter(), script.ParsedScript, script.RelativeScriptFilePath, script.ParentFragmentProvider, context.DefaultSchemaName);
         var column = resolver.Resolve(columnReference);
         if (column is null)
         {
             return;
+        }
+
+        var isTempTable = column.TableName.StartsWith('#');
+        if (isTempTable)
+        {
+            return;
+        }
+
+        var batch = script.ParentFragmentProvider.GetParents(columnReference).OfType<TSqlBatch>().FirstOrDefault();
+        if (batch is not null)
+        {
+            if (cteBatchInformationProvider.IsCte(batch, column.TableName))
+            {
+                return;
+            }
         }
 
         if (DoesTableColumnOrViewColumnExist(databasesByName, column.DatabaseName, column.SchemaName, column.TableName, column.ColumnName))
@@ -62,7 +87,7 @@ public sealed class MissingObjectAnalyzer : IGlobalAnalyzer
         context.IssueReporter.Report(DiagnosticDefinitions.Default, databaseName, script.RelativeScriptFilePath, fullObjectName, columnReference.GetCodeRegion(), "column", column.FullName);
     }
 
-    private static void AnalyzeTableReferences(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName)
+    private static void AnalyzeTableReferences(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName, CteBatchInformationProvider cteBatchInformationProvider)
     {
         var namedTableReferencesAndScripts = context.Scripts
             .SelectMany(a => a
@@ -73,17 +98,32 @@ public sealed class MissingObjectAnalyzer : IGlobalAnalyzer
 
         foreach (var (tableReference, script) in namedTableReferencesAndScripts)
         {
-            AnalyzeTableReference(context, settings, databasesByName, script, tableReference);
+            AnalyzeTableReference(context, settings, databasesByName, script, tableReference, cteBatchInformationProvider);
         }
     }
 
-    private static void AnalyzeTableReference(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName, IScriptModel script, NamedTableReference tableReference)
+    private static void AnalyzeTableReference(IAnalysisContext context, Aj5044Settings settings, IReadOnlyDictionary<string, DatabaseInformation> databasesByName, IScriptModel script, NamedTableReference tableReference, CteBatchInformationProvider cteBatchInformationProvider)
     {
+        var tableName = tableReference.SchemaObject.BaseIdentifier.Value;
+        var isTempTable = tableName.StartsWith('#');
+        if (isTempTable)
+        {
+            return;
+        }
+
+        var batch = script.ParentFragmentProvider.GetParents(tableReference).OfType<TSqlBatch>().FirstOrDefault();
+        if (batch is not null)
+        {
+            if (cteBatchInformationProvider.IsCte(batch, tableReference.SchemaObject.BaseIdentifier.Value))
+            {
+                return;
+            }
+        }
+
+        var schemaName = tableReference.SchemaObject.SchemaIdentifier?.Value.NullIfEmptyOrWhiteSpace() ?? context.DefaultSchemaName;
         var databaseName = tableReference.SchemaObject.DatabaseIdentifier?.Value.NullIfEmptyOrWhiteSpace()
                            ?? script.ParsedScript.TryFindCurrentDatabaseNameAtFragment(tableReference)
                            ?? DatabaseNames.Unknown;
-        var schemaName = tableReference.SchemaObject.SchemaIdentifier?.Value.NullIfEmptyOrWhiteSpace() ?? context.DefaultSchemaName;
-        var tableName = tableReference.SchemaObject.BaseIdentifier.Value;
 
         if (DoesTableOrViewExist(databasesByName, databaseName, schemaName, tableName))
         {
@@ -210,6 +250,31 @@ public sealed class MissingObjectAnalyzer : IGlobalAnalyzer
         }
 
         public IReadOnlyList<IIssue> Issues => [];
+    }
+
+    private sealed class CteBatchInformationProvider
+    {
+        private readonly IReadOnlyDictionary<TSqlBatch, FrozenSet<string>> _cteNamesByBatch;
+
+        public CteBatchInformationProvider(IReadOnlyDictionary<TSqlBatch, FrozenSet<string>> cteNamesByBatch)
+        {
+            _cteNamesByBatch = cteNamesByBatch;
+        }
+
+        public bool IsCte(TSqlBatch? batch, string? tableName)
+        {
+            if (batch is null || tableName.IsNullOrWhiteSpace())
+            {
+                return false;
+            }
+
+            if (!_cteNamesByBatch.TryGetValue(batch, out var cteNames))
+            {
+                return false;
+            }
+
+            return cteNames.Contains(tableName);
+        }
     }
 
     private static class DiagnosticDefinitions
