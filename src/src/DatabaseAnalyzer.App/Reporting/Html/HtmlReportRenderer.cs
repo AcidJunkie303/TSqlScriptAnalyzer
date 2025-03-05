2b04975d28db1e77aaefaee221f033f6197195c4
AcidJunkie303 <thomas_binggeli@yahoo.com>
2025-03-05 06:28:37 +0100

    HTML report: support dark and light themes #47 (#137)

--- src/src/DatabaseAnalyzer.App/Reporting/Html/HtmlReportRenderer.cs
+++ src/src/DatabaseAnalyzer.App/Reporting/Html/HtmlReportRenderer.cs
@@ -5,5 +5,19 @@ namespace DatabaseAnalyzer.App.Reporting.Html;
 
 internal sealed class HtmlReportRenderer : IReportRenderer
 {
-    public Task<string> RenderReportAsync(AnalysisResult analysisResult) => RazorTemplateEngine.RenderAsync("Reporting/Html/HtmlReport.cshtml", analysisResult);
+    private readonly ReportTheme _theme;
+
+    public HtmlReportRenderer(ReportTheme theme)
+    {
+        _theme = theme;
+    }
+
+    public Task<string> RenderReportAsync(AnalysisResult analysisResult)
+    {
+        var viewBag = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "Theme", _theme }
+        };
+        return RazorTemplateEngine.RenderAsync("Reporting/Html/HtmlReport.cshtml", viewModel: analysisResult, viewBagOrViewData: viewBag);
+    }
 }
