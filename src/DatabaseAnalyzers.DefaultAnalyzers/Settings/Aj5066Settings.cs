95bbf0431fdc3b842e3fc656b0d62b09f296e915
AcidJunkie303 <thomas_binggeli@yahoo.com>
2025-03-24 18:59:04 +0100

    Added TableReferenceWithoutSchemaAnalyzer (#314)

---
+++ src/DatabaseAnalyzers.DefaultAnalyzers/Settings/Aj5066Settings.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using DatabaseAnalyzer.Common.Contracts;
+using DatabaseAnalyzer.Common.Contracts.Settings;
+using DatabaseAnalyzer.Common.Extensions;
+
+namespace DatabaseAnalyzers.DefaultAnalyzers.Settings;
+
+// ReSharper disable once UnusedMember.Global -> is used for setting deserialization
+[SettingsSource(SettingsSourceKind.Diagnostics, "AJ5066")]
+internal sealed class Aj5066SettingsRaw : IRawDiagnosticSettings<Aj5066Settings>
+{
+    [Description("Table names to ignore. Wildcards like `*` and `?` are supported.")]
+    public IReadOnlyCollection<string?>? IgnoredTableNames { get; set; }
+
+    public Aj5066Settings ToSettings() => new
+    (
+        IgnoredTableNames
+            .EmptyIfNull()
+            .WhereNotNullOrWhiteSpaceOnly()
+            .Select(a => a.ToRegexWithSimpleWildcards(caseSensitive: false, compileRegex: true))
+            .ToList()
+    );
+}
+
+public sealed record Aj5066Settings(
+    IReadOnlyList<Regex> IgnoredTableNames
+) : IDiagnosticSettings<Aj5066Settings>
+{
+    public static Aj5066Settings Default { get; } = new([]);
+
+    public static string DiagnosticId => "AJ5066";
+}
